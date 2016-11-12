9e66b61b1c3e690a50866b183de13c326a672847
Bhavin <bhavin.m91@gmail.com>
2016-11-12 17:52:44 -0500

    Modified to handle major events like Super Bowl and Confedration Cup for NFL

--- nfl_schdule_parsing.R
+++ nfl_schdule_parsing.R
@@ -2,6 +2,12 @@ setwd("D:/Fall 2016/Data_Science/Project Data Set/NFL Calendar")
 
 raw_schedule <- read.csv("NFLSchedule_1316.csv")
 raw_schedule = raw_schedule[-1]
+raw_schedule$Week = as.character(raw_schedule$Week)
+
+raw_schedule$Week = ifelse(raw_schedule$Week == "WildCard",18,raw_schedule$Week)
+raw_schedule$Week = ifelse(raw_schedule$Week == "Division",19,raw_schedule$Week)
+raw_schedule$Week = ifelse(raw_schedule$Week == "ConfChamp",20,raw_schedule$Week)
+raw_schedule$Week = ifelse(raw_schedule$Week == "SuperBowl",21,raw_schedule$Week)
 
 raw_schedule$Week = as.numeric(raw_schedule$Week)
 raw_schedule$Date = as.Date(raw_schedule$Date)
@@ -12,8 +18,6 @@ colnames(measure_week) = c("Week","Date")
 #find unique values
 unq_measure_week = unique(measure_week)
 
-
-library(lubridate)
 nextdates = data.frame(unq_measure_week$Week,unq_measure_week$Date + 1)
 colnames(nextdates) = c("Week","Date")
 
